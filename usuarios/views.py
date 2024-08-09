fd92123420993db47d71d1408ad567a4eec4baaa
LeylaRegina <leylareginaa@gmail.com>
2024-08-08 20:27:12 -0400

    atualizacao 08082024

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate #isso serve para o proprio django ir no bd pegar as info e confirmar#
+from django.contrib.auth import login as login_django #so faz liberação das autenticação, não consulta o bd#
 
-# Create your views here.
+def login(request):
+    if request.method == "GET":
+        return render(request, 'usuarios/login.html')
+    else:
+        usermane = request.POST.get('email')
+        senha = request.POST.get('senha')
+
+        user = authenticate(usermane = usermane, password = senha)
+
+        if user:
+            login_django(request, user)
+            return HttpResponse('Autenticado!')
+        else:
+            return HttpResponse('E-mail ou senha invalidos!')
